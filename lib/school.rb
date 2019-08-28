f6c610b7f38e1d793199c208ce02dcd00f88e1ac
Katie Seggelke <katie.seggelke@gmail.com>
2019-08-28 19:04:58 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,9 +1,18 @@
 class School 
   
+  roster = {}
   def initialize(name)
     @name = name 
   end 
   
-  
+  def add_student(name, grade)
+    if roster == :grade
+      roster[:grade] = []
+      roster[:grade] << name 
+    else 
+      roster[:grade] = []
+      roster[:grade] << name
+    end 
+  end 
   
 end 
